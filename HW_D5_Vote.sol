9d1faee905d7fef808e174cbb4c673c7dbfe2190
andysfc <56067403+andysfc@users.noreply.github.com>
2019-11-27 16:36:53 +0800

    Create HW_D5_Vote.sol

---
+++ HW_D5_Vote.sol
@@ -0,0 +1,21 @@
+pragma solidity >=0.4.22 <0.6.0;
+
+contract State {
+
+    
+    constructor() public {
+
+    }
+    
+    function vote() public returns () {
+
+    }
+    
+    function getResult() public view returns ( ) {
+
+    }
+    
+    function getLastOne() public view returns ( ) {
+
+    }
+}
