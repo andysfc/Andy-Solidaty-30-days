8037e539cc315a04e1d93d20b0e939832382b367
andysfc <56067403+andysfc@users.noreply.github.com>
2020-04-29 16:23:41 +0800

    Create HW_D15_abstract.sol

---
+++ HW_D15_abstract.sol
@@ -0,0 +1,41 @@
+pragma solidity >=0.4.22 <0.6.0;
+
+contract Bank {
+    mapping (address => uint256) _depositors;
+    constructor() internal {
+    }
+    
+    function Deposit(uint256 count) public {
+        _Deposit(count);
+    }
+    
+    function Withdraw(uint256 count) public {
+        _Withdraw(count);
+    }
+    
+    function _Deposit(uint256 count) public internal {
+        _depositors[msg.sender] += count;
+    }
+    
+    function _Withdraw(uint256 count) public internal {
+        require(count <= _depositors[msg.sender], "Insufficient balance.")
+        _depositors[msg.sender] -= count;
+    }
+    
+    function GetBalance() public view returns (uint256) {
+        return _depositors[msg.sender];
+    }
+}
+
+contract SimpleBank is Bank {
+}
+
+contract HimoBank is Bank {
+    function Deposit(uint256 count) public {
+        _Deposit(count - 10);
+    }
+    
+    function Withdraw(uint256 count) public {
+        _Withdraw(count - 10);
+    }
+}
