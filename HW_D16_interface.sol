987d879c5ec705a31d9a520e42c1f00cb8e0483a
andysfc <56067403+andysfc@users.noreply.github.com>
2020-05-06 14:30:16 +0800

    Create HW_D16_interface.sol

---
+++ HW_D16_interface.sol
@@ -0,0 +1,64 @@
+pragma solidity >=0.4.22 <0.6.0;
+
+interface BankIntf {
+    // TODO
+    function Deposit() external payable returns (uint256 balance);
+    function Withdraw(uint256 valueInWei) external returns (uint256 balance);
+    function GetBalance() external view returns (uint256);
+}
+
+contract AbstractBank is BankIntf{
+    mapping (address => uint256) _depositors;
+    
+    constructor() internal { }
+    
+    function _Deposit(uint256 count) internal {
+        _depositors[msg.sender] += count;
+    }
+    
+    function _Withdraw(uint256 count) internal {
+        require(count <= _depositors[msg.sender], "Insufficient balance.");
+        _depositors[msg.sender] -= count;
+    }
+    
+    function GetBalance() public view returns (uint256) {
+        return _depositors[msg.sender];
+    }
+    
+    function GetBankBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+}
+
+contract SimpleBank is AbstractBank {
+    function Deposit() public payable returns (uint256 balance) {
+        // TODO
+        _Deposit(msg.value);
+        return GetBalance();
+    }
+    
+    function Withdraw(uint256 valueInWei) public returns (uint256 balance) {
+        // TODO
+        require(address(this).balance >= valueInWei);
+        _Withdraw(valueInWei);
+        msg.sender.transfer(valueInWei);
+        return GetBalance();
+    }
+}
+
+contract HimoBank is AbstractBank {
+    // TODO
+    // Charge 1% fee for Deposit & Withdraw
+    function Deposit() public payable returns (uint256 balance) {
+        _Deposit(msg.value*99/100);
+        return GetBalance();
+    }
+    
+    function Withdraw(uint256 valueInWei) public returns (uint256 balance) {
+        uint256 totalWithdraw = valueInWei * 101/100;
+        require(address(this).balance >= valueInWei);
+        _Withdraw(totalWithdraw);
+        msg.sender.transfer(valueInWei);
+        return GetBalance();
+    }
+}
